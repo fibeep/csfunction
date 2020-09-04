5bfa658bd4d5016b22c3a982adff00385ce5d382
Salo <salomonco@gmail.com>
2020-09-03 20:20:59 -0400

    first commit

---
+++ functions.py
@@ -0,0 +1,27 @@
+def area(width, height):
+    result = width * height
+    return result
+
+
+result = area(5, 6)
+print(result)
+
+result2 = area(10,20)
+result3 = area(2,18)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result4 = subtract(10,5)
+print(result4)
+
+result5 = subtract(20,7)
+print(result5)
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+print(add(2,3))
+print(add(4,5))
